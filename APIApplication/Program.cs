63af5b756745abdd587982796d6b7c110d2ce259
Bimhanguelo <best.imhanguelo@students.plymouth.ac.uk>
2024-01-09 14:05:56 +0000

    changes made

--- APIApplication/Program.cs
+++ APIApplication/Program.cs
@@ -12,11 +12,11 @@ var config = provider.GetRequiredService<IConfiguration>();
 builder.Services.AddDbContext<APIAPPContext>(item => item.UseSqlServer(config.GetConnectionString("con")));
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 app.UseSession();
